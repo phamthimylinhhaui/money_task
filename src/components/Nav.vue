18e3062f432163bc5e650230edb78dce0544344a
nấm <87581534+phamthimylinhhaui@users.noreply.github.com>
2023-10-28 12:11:54 +0700

    7-Route (#1)
    * learn/404page:all link not found to redirect home
    
    * fix: url has #
    
    * route name
    
    ---------
    
    Co-authored-by: hapo-linhptm <linh.ptm@haposoft.com>

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,7 @@
 <template>
   <nav class="nav">
-    <router-link to="/" class="nav-link">Over view</router-link>
-    <router-link to="/transaction" class="nav-link">Transaction</router-link>
+    <router-link :to="{ name: 'home' }" class="nav-link">Over view</router-link>
+    <router-link :to="{ name: 'transaction' }" class="nav-link">Transaction</router-link>
+    <router-link to="/about" class="nav-link">About</router-link>
   </nav>
 </template>
--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/pages/home";
 import Transaction from "@/pages/transaction.vue";
 import TransactionDetail from "@/pages/transaction-detail.vue";
@@ -6,24 +6,31 @@ import TransactionDetail from "@/pages/transaction-detail.vue";
 const routes = [
     {
       path: "/",
+      name: "home",
       component: Home,
     },
     {
         path: "/transaction",
+        name: "transaction",
         component: Transaction,
     },
     {
         path: "/transaction/:id",
+        name: "transaction-detail",
         component: TransactionDetail,
     },
     {
         path: "/ts",
-        redirect: '/transaction',
+        redirect: "/transaction",
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        component: Home,
     },
 ];
 
 const router = createRouter({
-    history: createWebHashHistory(process.env.BASE_URL),
+    history: createWebHistory(process.env.BASE_URL),
     routes,
 });
 
